0885d6646fc84dbb36559551a4c5ee495fbbefda
Łukasz Duda <luka.dd@wp.pl>
2026-01-21 19:12:23 +0100

    Pierwsza wersja - struktura projektu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Pierwszy skrypt
+console.log('Portfolio Łukasza Dudy - strona załadowana!');
+
+// Sprawdzenie czy strona się załadowała
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM gotowy do pracy!');
+});
